a15f2357b8465b966dfa525fd934ff0f9be959cc
CarlosViniciusBizinottoPrates <viniciusbizinotto@gmail.com>
2023-01-06 00:31:05 -0300

    Teste Commit android

---
+++ app/src/main/java/com/example/appidiomas/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.appidiomas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // O desafio é criar um valor dentro do string.xml
+        //E trocar o texto do xml e tornar internacional.
+    }
+}
